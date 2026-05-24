2de218289f0e500c0690e22f82aa58aaae95f4ec
季楠 <2759278070@qq.com>
2026-05-24 16:29:29 +0800

    feat: 初始化提交

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -9,7 +9,7 @@ export const ACTION_TAG_LABELS: Record<string, string> = {
 }
 
 export const CONNECTION_TAG_LABELS: Record<string, string> = {
-  place: '当前地点',
+  place: '附近地点',
   area: '其他区域',
   other: '移动',
 }
